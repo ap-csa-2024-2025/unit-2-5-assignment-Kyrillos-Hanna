a817af4841f40e8680fba83811d113a3e16abdf3
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-10-07 16:55:11 -0700

    started 2.5 hw

--- Main.java
+++ Main.java
@@ -4,6 +4,17 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+  Circle c1 = new Circle(10.1);
+  Circle c2 = new Circle(14);
+  Circle c3 = new Circle(20.5);
+
+  System.out.println(c1.toString());
+  System.out.println(c2.toString());
+  System.out.println(c3.toString());
+
+  }
+
+  public static void threeRectangles(double firstLength, double secondLength, double thirdLength) {
+    
   }
 }
