a1a838e5a1cd3766524c017933754e0b1fc51a72
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-10-08 10:38:09 -0700

    finished hw

--- Main.java
+++ Main.java
@@ -12,9 +12,34 @@ public class Main
   System.out.println(c2.toString());
   System.out.println(c3.toString());
 
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Type a number for length and width: ");
+  double len1 = sc.nextDouble();
+  System.out.println("Type a number for length: ");
+  double len2 = sc.nextDouble();
+  System.out.println("Type a number for width: ");
+  double len3 = sc.nextDouble();
+
+  threeRectangles(len2, len3, len1);
+
+  System.out.println("Type a side length: ");
+  double length = sc.nextDouble();
+  RegularPolygon triangle = new RegularPolygon(3, length);
+  RegularPolygon square = new RegularPolygon(4, length);
+  System.out.println(triangle.toString());
+  System.out.println(square.toString());
+
+
+
+  }
+  public static void threeRectangles(double firstLength, double secondLength, double thirdLength) {
+    Rectangle r1 = new Rectangle(firstLength, secondLength);
+    Rectangle r2 = new Rectangle(thirdLength);
+
+    System.out.println(r1.toString());
+    System.out.println(r2.toString());
   }
 
-  public static void threeRectangles(double firstLength, double secondLength, double thirdLength) {
-    
   }
-}
+
+
